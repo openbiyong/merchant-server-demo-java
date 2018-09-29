0dccacd3c66e61ea543a66ad017e085a49da979d
Sion <zsion@vip.qq.com>
2018-09-29 14:13:25 +0800

    allow cross-domain request

--- src/main/java/com/biyong/open/server/Application.java
+++ src/main/java/com/biyong/open/server/Application.java
@@ -2,12 +2,34 @@ package com.biyong.open.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @ComponentScan(basePackages = "com.biyong")
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  /**
+   * TODO 这里运行跨域访问，方便测试
+   */
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", buildConfig()); // 4
+    return new CorsFilter(source);
+  }
+
+  private CorsConfiguration buildConfig() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.addAllowedOrigin("*");
+    corsConfiguration.addAllowedHeader("*");
+    corsConfiguration.addAllowedMethod("*");
+    return corsConfiguration;
   }
 }
